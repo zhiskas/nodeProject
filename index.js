c59a14597da6630512504cc5044d2fabb3cea675
iasonasxrist <iasonasxrist@gmail.com>
2024-03-16 13:52:50 +0200

    .env

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const products_api = require("./routes/products_api");
 const students_api = require("./routes/students_api");
 require("./schema/Products");
 require("./schema/Student");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(products_api);
-app.use(students_api)
+app.use(students_api);
 // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // console.log(swaggerSpec);
 const port = process.env.PORT || 3000;
 
-const mongoUri =
-  "mongodb+srv://Zisiskats:shitheadshithead@zhiskas.uqttaxw.mongodb.net/students";
+const mongoUri = process.env.URL;
 
 mongoose.connect(mongoUri);
 
